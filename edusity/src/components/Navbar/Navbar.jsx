b6ab83464ac8c5f10be500677f03ad9d1774bb04
spriveen <rpeanuka@gmail.com>
2025-01-17 20:00:53 -0800

    update Btn for Navbar

--- edusity/src/components/Navbar/Navbar.jsx
+++ edusity/src/components/Navbar/Navbar.jsx
@@ -4,15 +4,15 @@ import logo from '../../assets/logo.png'
 
 const Navbar = () => {
   return (
-    <nav>
-      <img src={logo} alt='' />
+    <nav className='container'>
+      <img src={logo} alt='' className='logo' />
       <ul>
         <li>Home</li>
         <li>Programme</li>
         <li>About us</li>
         <li>Campus</li>
         <li>Testimonials</li>
-        <li>Contact Us</li>
+        <li><button className='btn '>Contact Us</button></li>
 
       </ul>
     </nav>

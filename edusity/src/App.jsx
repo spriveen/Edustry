a58a43a3ed9f426c79cf437bfd2cc292a3458d06
spriveen <rpeanuka@gmail.com>
2025-01-18 07:04:51 -0800

    update Program

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import Programs from './components/Programs/Programs'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className='container'>
+         <Programs/>
+      </div>
       
-      
-    </div>
+  </div>
   )
 }
 

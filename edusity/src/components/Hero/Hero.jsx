6c2a9c0b82871490021faae2e8dda8f5a82ef50d
spriveen <rpeanuka@gmail.com>
2025-01-17 20:43:51 -0800

    update hero section

--- edusity/src/components/Hero/Hero.jsx
+++ edusity/src/components/Hero/Hero.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Hero.css'
+import dark_arrow from '../../assets/dark-arrow.png'
 
 const Hero = () => {
   return (
-    <div className='hero'>
-      
+    <div className='hero container'>
+      <div className='hero-text'>
+        <h1>We provide quality education to build a better world.</h1>
+        <p>Our mission is to deliver exceptional education that empowers individuals and transforms communities, paving the way for a brighter and more prosperous world.</p>
+        <button className='btn'>Explore More <img src={dark_arrow} alt='' /></button>
+        
+
+      </div>
     </div>
   )
 }

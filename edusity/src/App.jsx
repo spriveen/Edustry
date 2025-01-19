239e753b91ec164851f43968b47bdc840c35226f
spriveen <rpeanuka@gmail.com>
2025-01-19 05:24:06 -0800

    update Campus.jsx

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero/Hero'
 import Programs from './components/Programs/Programs'
 import Title from './components/Title/Title'
 import About from './components/About/About'
+import Campus from './components/Campus/Campus'
 
 
 const App = () => {
@@ -16,6 +17,8 @@ const App = () => {
          <Programs/>
          <About/>
          <Title subTitle='Gallery' title='Campus Photoes'/>
+         <Campus />
+         <Title subTitle='TESTIMONIALS' title='What students Says'/>
       </div>
       
   </div>
